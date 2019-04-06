7a86e8c0e67c5f2815c67cebebb67902c2f53589
burasDiana <burasdiana@hotmail.com>
2019-04-06 15:58:28 +0200

    initial commit

---
+++ backend_service/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/diana/Documents/hackathonproject/CopenHack2019/backend_service/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
